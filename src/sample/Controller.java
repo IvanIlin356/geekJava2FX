6bdf66500891aa1fbd7ae76640d53af666286b68
Ivan Ilin <vanilin356@gmail.com>
2018-03-10 15:00:51 +0300

    template done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+
 public class Controller {
+    @FXML
+    TextArea conversationArea;
+    @FXML
+    TextField messageText;
+
+    public void sendMessage(){
+        if (!messageText.getText().isEmpty()) {
+            conversationArea.appendText(messageText.getText() + "\n");
+            messageText.clear();
+            messageText.requestFocus();
+        }
+    }
 }
